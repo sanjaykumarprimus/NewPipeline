1ea0190bae1697d279a2bdb44a5f04cb2add1f33
Sanjay Kumar <sanjay.kumar@primussoft.com>
2019-08-20 15:16:38 +0530

    First Commit from VS.

--- CICDAPI/Controllers/ValuesController.cs
+++ CICDAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CICDAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Sanjay", "Kumar" };
         }
 
         // GET api/values/5
