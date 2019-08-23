53a2517c2829f4ee5b81af6cfefce948d08c5afe
Sanjay Kumar <sanjay.kumar@primussoft.com>
2019-08-23 17:14:42 +0530

    Test Changes

--- CICDAPI/Controllers/ValuesController.cs
+++ CICDAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace CICDAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Hello", "Sanjay" };
+            return new string[] { "Hello", "Sanjay " };
         }
 
         // GET api/values/5
